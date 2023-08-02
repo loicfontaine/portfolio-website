97587db1dd43e13a9f9e57368cc75eb56044fb43
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-08-02 15:54:37 +0200

    first real commit

---
+++ src/index.js
@@ -0,0 +1,236 @@
+const icons = {
+  github: `<svg width="83" height="81" viewBox="0 0 83 81" xmlns="http://www.w3.org/2000/svg">
+  <path d="M41.5 0C36.0501 0 30.6536 1.07357 25.6186 3.15942C20.5836 5.24526 16.0087 8.30254 12.1551 12.1567C4.37231 19.9405 0 30.4976 0 41.5055C0 59.8509 11.9105 75.4155 28.386 80.9357C30.461 81.2678 31.125 79.9811 31.125 78.8604V71.846C19.6295 74.3363 17.181 66.2843 17.181 66.2843C15.272 61.4696 12.5745 60.183 12.5745 60.183C8.798 57.6096 12.865 57.6926 12.865 57.6926C17.015 57.9832 19.2145 61.9677 19.2145 61.9677C22.825 68.2765 28.9255 66.4088 31.291 65.4127C31.6645 62.7148 32.7435 60.8886 33.9055 59.8509C24.6925 58.8133 15.023 55.2438 15.023 39.4302C15.023 34.8231 16.6 31.1291 19.2975 28.1822C18.8825 27.1446 17.43 22.828 19.7125 17.2248C19.7125 17.2248 23.1985 16.1041 31.125 21.4583C34.4035 20.5452 37.9725 20.0887 41.5 20.0887C45.0275 20.0887 48.5965 20.5452 51.875 21.4583C59.8015 16.1041 63.2875 17.2248 63.2875 17.2248C65.57 22.828 64.1175 27.1446 63.7025 28.1822C66.4 31.1291 67.977 34.8231 67.977 39.4302C67.977 55.2853 58.266 58.7718 49.0115 59.8094C50.5055 61.0961 51.875 63.6279 51.875 67.4879V78.8604C51.875 79.9811 52.539 81.3093 54.6555 80.9357C71.131 75.374 83 59.8509 83 41.5055C83 36.0549 81.9266 30.6577 79.841 25.622C77.7554 20.5863 74.6986 16.0108 70.8449 12.1567C66.9913 8.30254 62.4164 5.24526 57.3814 3.15942C52.3464 1.07357 46.9499 0 41.5 0Z"/>
+</svg>`,
+  linkedin: `<svg
+                width="89"
+                height="88"
+                viewBox="0 0 89 88"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M79.1111 0C81.7338 0 84.2491 1.03016 86.1036 2.86384C87.9581 4.69753 89 7.18455 89 9.77778V78.2222C89 80.8155 87.9581 83.3025 86.1036 85.1362C84.2491 86.9698 81.7338 88 79.1111 88H9.88889C7.26619 88 4.75091 86.9698 2.89639 85.1362C1.04186 83.3025 0 80.8155 0 78.2222V9.77778C0 7.18455 1.04186 4.69753 2.89639 2.86384C4.75091 1.03016 7.26619 0 9.88889 0H79.1111ZM76.6389 75.7778V49.8667C76.6389 45.6397 74.9407 41.5859 71.9178 38.597C68.8949 35.608 64.795 33.9289 60.52 33.9289C56.3172 33.9289 51.4222 36.4711 49.0489 40.2844V34.8578H35.2539V75.7778H49.0489V51.6756C49.0489 47.9111 52.1144 44.8311 55.9217 44.8311C57.7576 44.8311 59.5183 45.5522 60.8164 46.8358C62.1146 48.1194 62.8439 49.8603 62.8439 51.6756V75.7778H76.6389ZM19.1844 27.1822C21.3875 27.1822 23.5003 26.3169 25.0581 24.7766C26.6159 23.2363 27.4911 21.1472 27.4911 18.9689C27.4911 14.4222 23.7828 10.7067 19.1844 10.7067C16.9683 10.7067 14.8429 11.5771 13.2758 13.1266C11.7087 14.6761 10.8283 16.7776 10.8283 18.9689C10.8283 23.5156 14.5861 27.1822 19.1844 27.1822ZM26.0572 75.7778V34.8578H12.3611V75.7778H26.0572Z"
+                />
+              </svg>`,
+  behance: `<svg width="81" height="81" viewBox="0 0 81 81"  xmlns="http://www.w3.org/2000/svg" class="behance-image">
+<g id="wpf:behance" clip-path="url(#clip0_157_64)">
+<path id="Vector" d="M60.75 38.6308C61.3731 39.5654 61.9962 40.5 62.3077 42.0577H52.3385C52.3385 41.7462 52.3385 41.1231 52.65 40.5C52.9615 39.8769 52.9615 39.5654 53.5846 38.9423C53.8962 38.3192 54.5192 38.0077 55.1423 37.6962C55.7654 37.3846 56.7 37.0731 57.6346 37.0731C58.8808 37.3846 60.1269 37.6962 60.75 38.6308ZM32.0885 36.45C32.7115 35.8269 33.3346 34.8923 33.3346 33.6462C33.3346 33.0231 33.3346 32.4 33.0231 31.7769C32.7115 31.4654 32.4 30.8423 32.0885 30.8423C31.7769 30.5308 31.1538 30.5308 30.5308 30.2192H22.4308V37.3846H29.2846C30.2192 37.3846 31.1538 37.0731 32.0885 36.45ZM29.5962 41.7462H22.4308V50.1577H29.5962C30.2192 50.1577 30.8423 50.1577 31.4654 49.8462C32.0885 49.8462 32.7115 49.5346 33.0231 49.2231C33.3346 48.9115 33.9577 48.6 34.2692 47.9769C34.5808 47.3538 34.5808 46.7308 34.5808 46.1077C34.5808 44.55 34.2692 43.3038 33.3346 42.6808C32.4 42.0577 31.1538 41.7462 29.5962 41.7462ZM81 15.2654V65.7346C81 74.1462 74.1462 81 65.7346 81H15.2654C6.85385 81 0 74.1462 0 65.7346V15.2654C0 6.85385 6.85385 0 15.2654 0H65.7346C74.1462 0 81 6.85385 81 15.2654ZM51.0923 29.9077H63.5538V26.7923H51.0923V29.9077ZM41.1231 46.7308C41.1231 44.8615 40.8115 43.3038 39.8769 41.7462C38.9423 40.5 37.6962 39.5654 35.8269 38.9423C37.0731 38.3192 38.3192 37.3846 38.9423 36.45C39.5654 35.5154 39.8769 34.2692 39.8769 32.7115C39.8769 31.1538 39.5654 30.2192 39.2538 28.9731C38.6308 28.0385 38.0077 27.1038 37.3846 26.7923C36.45 26.1692 35.5154 25.8577 34.2692 25.5462C32.7115 24.9231 31.4654 24.9231 29.9077 24.9231H15.5769V55.4538H30.5308C31.7769 55.4538 33.3346 55.1423 34.5808 54.8308C35.8269 54.5192 37.0731 53.8962 38.0077 53.2731C38.9423 52.65 39.8769 51.7154 40.5 50.4692C40.8115 49.5346 41.1231 48.2885 41.1231 46.7308ZM52.3385 45.7962H68.2269C68.2269 43.9269 68.2269 42.3692 67.9154 40.8115C67.6038 39.2538 66.9808 37.6962 66.0462 36.7615C65.1115 35.5154 63.8654 34.5808 62.6192 33.9577C61.0615 33.3346 59.5038 32.7115 57.6346 32.7115C56.0769 32.7115 54.5192 33.0231 52.9615 33.6462C51.7154 34.2692 50.4692 35.2038 49.2231 36.1385C48.2885 37.0731 47.3538 38.3192 47.0423 39.8769C46.4192 41.4346 46.1077 42.9923 46.1077 44.55C46.1077 46.1077 46.4192 47.9769 47.0423 49.2231C47.6654 50.7808 48.2885 52.0269 49.2231 52.9615C50.1577 53.8962 51.4038 54.8308 52.65 55.4538C54.2077 56.0769 55.7654 56.3885 57.3231 56.3885C59.8154 56.3885 61.9962 55.7654 63.8654 54.5192C65.7346 53.2731 66.9808 51.4038 67.9154 48.9115H62.6192C62.3077 49.5346 61.9962 50.1577 61.0615 50.7808C60.1269 51.4038 59.1923 51.7154 57.9462 51.7154C56.3885 51.7154 54.8308 51.4038 53.8962 50.4692C52.9615 49.2231 52.3385 47.6654 52.3385 45.7962Z"/>
+</g>
+<defs>
+<clipPath id="clip0_157_64">
+<rect width="81" height="81" />
+</clipPath>
+</defs>
+</svg>
+`,
+  mail: `<svg width="81" height="65" viewBox="0 0 81 65" xmlns="http://www.w3.org/2000/svg">
+  <path d="M72.9 0H8.1C3.645 0 0.0405 3.65625 0.0405 8.125L0 56.875C0 61.3438 3.645 65 8.1 65H72.9C77.355 65 81 61.3438 81 56.875V8.125C81 3.65625 77.355 0 72.9 0ZM72.9 16.25L40.5 36.5625L8.1 16.25V8.125L40.5 28.4375L72.9 8.125V16.25Z"/>
+</svg>`,
+  illustrator: `<svg
+              width="35"
+              height="35"
+              viewBox="0 0 35 35"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M23.2342 14.7919V22.6042H21.245V14.7919H23.2342ZM21.1342 12.6102C21.1342 12.0006 21.5819 11.5354 22.2396 11.5354C22.9148 11.5354 23.3304 12.0006 23.3479 12.6102C23.3479 13.2037 22.9133 13.6689 22.225 13.6689C21.5673 13.6689 21.1342 13.2037 21.1342 12.6102Z"
+                fill="#826D00"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18.4246 22.6042L17.446 19.5402H14.0131L13.0827 22.6042H11.0454L14.5104 11.7921H17.029L20.5421 22.6042H18.4246ZM15.1681 15.3854L14.334 18.0484H17.1092L16.259 15.3854C16.0898 14.8532 15.9425 14.2684 15.8069 13.7332C15.776 13.6101 15.7449 13.4871 15.7135 13.3642H15.6815C15.626 13.5786 15.5721 13.8075 15.5152 14.0423C15.4073 14.4944 15.295 14.964 15.1681 15.3854Z"
+                fill="#826D00"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M25.1679 4.13149C20.0716 3.56652 14.9284 3.56652 9.83207 4.13149C6.89791 4.45961 4.52811 6.77107 4.18395 9.71982C3.57931 14.889 3.57931 20.1111 4.18395 25.2802C4.52811 28.229 6.89645 30.5404 9.83207 30.8686C14.9284 31.433 20.0715 31.433 25.1679 30.8686C28.1021 30.5404 30.4719 28.229 30.816 25.2802C31.4206 20.111 31.4206 14.889 30.816 9.71982C30.4719 6.77107 28.1035 4.45961 25.1679 4.13149ZM10.0742 6.3044C15.0096 5.75734 19.9904 5.75734 24.9258 6.3044C26.8654 6.52315 28.4185 8.05295 28.6446 9.97503C29.2293 14.9747 29.2293 20.0254 28.6446 25.025C28.5277 25.9657 28.097 26.8395 27.4223 27.5053C26.7475 28.1711 25.868 28.5899 24.9258 28.6942C19.9904 29.2413 15.0096 29.2413 10.0742 28.6942C9.13199 28.5899 8.25248 28.1711 7.57771 27.5053C6.90295 26.8395 6.47232 25.9657 6.35541 25.025C5.77068 20.0254 5.77068 14.9747 6.35541 9.97503C6.47232 9.03434 6.90295 8.16054 7.57771 7.49477C8.25248 6.829 9.13199 6.40869 10.0742 6.3044Z"
+                fill="#826D00"
+              />
+            </svg>`,
+  wordpress: ` <svg
+              width="27"
+              height="28"
+              viewBox="0 0 27 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.78605 14C1.78605 18.6413 4.48605 22.64 8.39025 24.5408L2.80395 9.23315C2.13296 10.7328 1.7861 12.3571 1.78605 14ZM13.5 25.714C14.8263 25.7123 16.1429 25.4877 17.3948 25.0497L17.3111 24.8932L13.7065 15.0287L10.1966 25.236C11.2361 25.5466 12.3471 25.714 13.5 25.714ZM15.1092 8.5055L19.3468 21.101L20.52 17.1968C21.0195 15.5768 21.4029 14.4144 21.4029 13.4073C21.4029 11.9534 20.8832 10.9557 20.4471 10.1876C19.845 9.21155 19.2942 8.3921 19.2942 7.43495C19.2942 6.35495 20.1042 5.35865 21.2679 5.35865H21.4123C19.254 3.37778 16.4296 2.28099 13.5 2.28605C11.56 2.28586 9.65032 2.7679 7.94281 3.68882C6.23529 4.60974 4.78346 5.94065 3.7179 7.56185L4.4658 7.5821C5.69025 7.5821 7.58025 7.42685 7.58025 7.42685C8.2242 7.3958 8.2971 8.3192 7.66395 8.3921C7.66395 8.3921 7.02945 8.4758 6.31395 8.50685L10.5935 21.1968L13.1585 13.5127L11.3292 8.50685C10.9201 8.4842 10.5118 8.44954 10.1047 8.4029C9.47025 8.3624 9.54315 7.3958 10.1763 7.42685C10.1763 7.42685 12.1082 7.5821 13.2611 7.5821C14.4868 7.5821 16.3769 7.42685 16.3769 7.42685C17.01 7.3958 17.0924 8.3192 16.4592 8.3921C16.4592 8.3921 15.8261 8.465 15.1092 8.50685V8.5055ZM19.3887 24.1237C22.0282 22.5894 23.9643 20.0852 24.7846 17.1444C25.6049 14.2037 25.2445 11.0589 23.7803 8.37995C23.9658 10.2194 23.6874 12.0759 22.9702 13.78L19.3874 24.125L19.3887 24.1237ZM13.5 27.5C9.91958 27.5 6.4858 26.0777 3.95406 23.5459C1.42232 21.0142 0 17.5804 0 14C0 10.4196 1.42232 6.9858 3.95406 4.45406C6.4858 1.92232 9.91958 0.5 13.5 0.5C17.0804 0.5 20.5142 1.92232 23.0459 4.45406C25.5777 6.9858 27 10.4196 27 14C27 17.5804 25.5777 21.0142 23.0459 23.5459C20.5142 26.0777 17.0804 27.5 13.5 27.5Z"
+                fill="#826D00"
+              />
+            </svg>`,
+};
+
+const projects = [
+  {
+    title: "Sleep",
+    tools: [
+      {
+        title: "Illustrator",
+        icon: icons.illustrator,
+      },
+      { title: "Wordpress", icon: icons.wordpress },
+    ],
+    tags: ["Marketing", "UX Design"],
+    date: "07/08/20",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl vel pretium lectus quam. At quis risus sed vulputate odio ut enim. Scelerisque mauris pellentesque pulvinar pellentesque habitant. Purus non enim praesent elementum facilisis leo vel fringilla est. Dictum varius duis at consectetur lorem donec massa sapien faucibus. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Consectetur purus ut faucibus pulvinar ",
+    image: "https://via.placeholder.com/150",
+    links: [
+      {
+        title: "Voir le site",
+        link: "https://via.placeholder.com/150",
+      },
+      { title: "voir le code", link: "https://via.placeholder.com/150" },
+    ],
+  },
+  {
+    title: "Sleep",
+    tools: [
+      {
+        title: "Illustrator",
+        icon: icons.illustrator,
+      },
+      { title: "Wordpress", icon: icons.wordpress },
+    ],
+    tags: ["Marketing", "UX Design"],
+    date: "07/08/20",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl vel pretium lectus quam. At quis risus sed vulputate odio ut enim. Scelerisque mauris pellentesque pulvinar pellentesque habitant. Purus non enim praesent elementum facilisis leo vel fringilla est. Dictum varius duis at consectetur lorem donec massa sapien faucibus. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Consectetur purus ut faucibus pulvinar ",
+    image:
+      "https://s3-alpha-sig.figma.com/img/48aa/1cc0/246ee2bc6c15c59565475f033a75a71c?Expires=1691366400&Signature=c2ncmNnR40bQC~MNNvPCaqxa3YMrjNCavx3J-3iGaq1H49x61ehqbrGJ03SbzthmovlaLz~VmtXswDHlUxp9ik8ETixqVmzf1WQSoSSmtwmF~HgJx1CxpclrOMGl8Jm4K2wGpkl7BapEZiw1DN6HI-sGh3wEEhN2SsUPxwXDLZwAo1KXrpVE-tIXXKcgB4H7WmNWchqw0T0Dp02YevRoiBm-55kSMoHhCaFdBzt--EMMLXZhvXZ1SMB02LNn2Y9697wjmafoZwhdjzgH6cxTTJ7L7k~9MkHbJ4-avbLdrWwIePmkHRio-haPzAiWDT~19ZQMh02feYNp1ijqyqdo3g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    links: [
+      {
+        title: "Voir le site",
+        link: "https://via.placeholder.com/150",
+      },
+      { title: "voir le code", link: "https://via.placeholder.com/150" },
+    ],
+  },
+];
+
+//PAS OUBLIER D'ENLEVER LE FILL DU PATH ET DU SVG
+
+const contacts = [
+  {
+    title: "linkedin/loic-fontaine-suisse",
+    link: "https://www.linkedin.com/in/loic-fontaine-suisse/",
+    icon: icons.linkedin,
+  },
+  {
+    title: "github.com/loicfontaine",
+    link: "https://github.com/loicfontaine",
+    icon: icons.github,
+  },
+  {
+    title: "contact@lfontaine.ch",
+    link: "mailto:contact@lfontaine.ch",
+    icon: icons.mail,
+  },
+
+  {
+    title: "behance.net/loic-fontaine",
+    link: "https://www.behance.net/loic-fontaine",
+    icon: icons.behance,
+  },
+];
+
+console.log("hey");
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("header");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+const projectsContainer = document.querySelector(".projects-container");
+const projectTemplate = document.querySelector("#project-template");
+const contactTemplate = document.querySelector("#contact-template");
+
+projects.forEach((project) => {
+  const newProject = projectTemplate.content.cloneNode(true);
+  const tagTemplate = newProject.querySelector("#tag-template");
+  const linkTemplate = newProject.querySelector("#button-template");
+  const toolTemplate = newProject.querySelector("#tool-template");
+
+  newProject.querySelector(".description").innerText = project.description;
+  newProject.querySelector(".title").innerText = project.title;
+  newProject.querySelector(".date").innerText = project.date;
+  newProject.querySelector("img").src = project.image;
+
+  project.tags.forEach((tag) => {
+    const newTag = tagTemplate.content.cloneNode(true);
+    newTag.querySelector(".tag").innerText = tag;
+    newProject.querySelector(".tags-container").append(newTag);
+  });
+
+  project.links.forEach((link) => {
+    console.log("mamama");
+    const newLink = linkTemplate.content.cloneNode(true);
+    newLink.querySelector(".btn-text").innerText = link.title;
+    newLink.querySelector("a").href = link.link;
+    newProject.querySelector(".buttons-container").append(newLink);
+  });
+
+  project.tools.forEach((tool) => {
+    const newTool = toolTemplate.content.cloneNode(true);
+    console.log("new tool", newTool);
+    newTool.querySelector(".tool-text").innerText = tool.title;
+    newTool.querySelector(".tool-icon").innerHTML = tool.icon;
+    newProject.querySelector(".tools-container").append(newTool);
+  });
+  projectsContainer.append(newProject);
+});
+
+contacts.forEach((contact) => {
+  const newContact = contactTemplate.content.cloneNode(true);
+  newContact.querySelector("a").href = contact.link;
+  newContact.querySelector(".icon").innerHTML = contact.icon;
+  newContact.querySelector(".contact-text").innerText = contact.title;
+  document.querySelector(".contacts-container").append(newContact);
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
+
+var typed = new Typed(".typed", {
+  strings: [
+    "Ingénieur des médias",
+    "Développeur front-end",
+    "Développeur back-end",
+    "Designer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  backDelay: 2000,
+});
